08948f7e1b3c963019ba0cfa9a2309a9758cb48c
Tom <tombo.miller@gmail.com>
2018-06-22 14:55:13 +0100

    Wrap

--- medic.py
+++ medic.py
@@ -9,6 +9,7 @@ HEAL_RATE = 5 # Health healed per hit
 
 def apply_script(protocol, connection, config):
     class MedicConnection(connection):
+
         def __init__(self, *arg, **kw):
             connection.__init__(self, *arg, **kw)
 
@@ -18,8 +19,11 @@ def apply_script(protocol, connection, config):
             and self.weapon == SHOTGUN_WEAPON):
                 hit_player.set_hp(hit_player.hp + HEAL_RATE, type = MELEE_KILL)
                 if (hit_player.hp >= 100): # If the target has full health
-                     connection.send_chat(self.name + ' is all healed up!')
-            return connection.on_hit(self, hit_amount, hit_player,
-                type, grenade)
+                    connection.send_chat(hit_player.name + ' has %s health'
+                        % hit_player.hp)
+                else:
+                    connection.send_chat(hit_player.name + ' is all healed up!')
+            return connection.on_hit(self, hit_amount, hit_player, type
+                , grenade)
 
     return protocol, MedicConnection
